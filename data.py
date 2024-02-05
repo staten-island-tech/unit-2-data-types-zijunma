018a6170426420e2d0df077e010f62af47211b43
zijunma <158203969+zijunma@users.noreply.github.com>
2024-02-05 12:28:55 -0500

    Create data.py

---
+++ data.py
@@ -0,0 +1,14 @@
+#float is decimals, x is whole 
+""" x = 3
+y= float(3)
+print(x,y) """
+""" 
+values = [0,6,1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i) 
+
+print(values[0])
+print(values[6]) """
+
+give
