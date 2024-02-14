0ce79789523b11a819199844981e0b98bafb0347
zijunma <158203969+zijunma@users.noreply.github.com>
2024-02-14 13:08:16 -0500

    main

--- gcf.py
+++ gcf.py
@@ -1,12 +1,14 @@
-def find_gcf(num1,num2):
-     if num1 < num2:
-        num1 , num2 = num2 , num1
-     while num2  !=0:
-         num1 , num2 = num2 , num1 % num2
-         return num1
-     
-num1 = int(input("First Number:"))
-num2 = int(input("Second Number:"))
+def find_gcf(x,y):
+   if x > y:
+      variable1 = y
+   else:
+      variable1 = x
+   for i in range(1, variable1 + 1):
+      if((x % i == 0) and (y % i == 0)):
+         gcf = i
+   print([gcf])
 
-gcf = find_gcf(num1 , num2)
-print([gcf])
+x = int(input("number 1:"))
+y = int(input("number 2:"))
+print ("The gcf of your numbers is:")
+print (find_gcf(x,y))
