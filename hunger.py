931d2e69b078fafaca48214cf2140c8a08d6cbc4
zijunma <158203969+zijunma@users.noreply.github.com>
2024-02-15 17:21:49 -0500

    Update hunger.py

--- hunger.py
+++ hunger.py
@@ -1,18 +1,29 @@
-def calculate_remaining_hunger(initial_hungerpoints, seconds_waited, apples_eaten, hunger_gainedperapple, hunger_lostpersecond):
+def calculate_remaining_hunger(initial_hunger_points, apples_eaten, hunger_gained_per_apple, hunger_lost_per_second, waiting_seconds):
 
 # calc total number of hunger gained from eating apples
 
-totalhungerpointsgained = apples_eaten * initial_hungerpoints
+    total_hunger_points_gained = apples_eaten * initial_hunger_points
 
+# makes sure the hunger doesn't overflow the max hunger bar
+    
+    total_hunger_points_gained = min(total_hunger_points_gained, max_hunger_points - initial_hunger_points)
+ 
 # calculate the hunger points lost when waiting
  
-total_hunger_points_lost = hunger_lostpersecond * seconds_waited
+    total_hunger_points_lost = hunger_lost_per_second * waiting_seconds
 
 # calculate the final hunger level
 
-final_hunger = totalhungerpointsgained - total_hunger_points_lost
+    remaining_hunger_points = max(0, initial_hunger_points + total_hunger_points_gained - total_hunger_points_lost)
 
-hunger_lostpersecond = int(input())
-initial_hungerpoints = int(input())
-seconds_waited = int(input())
-apples_eaten = int(input())
+    return remaining_hunger_points
+
+max_hunger_points = int(input("Max Hunger Points:"))
+initial_hunger_points = int(input("Initial Hunger Points:"))
+apples_eaten = int(input("Apples Eaten:"))
+hunger_gained_per_apple = int(input("Hunger Gained Per Apple:"))
+hunger_lost_per_second = int(input("Hunger Lost Per Second Waiting:"))
+waiting_seconds = int(input("Seconds Waiting:"))
+ 
+result = calculate_remaining_hunger(initial_hunger_points, apples_eaten, hunger_gained_per_apple, hunger_lost_per_second, waiting_seconds)
+print("Bob's remaining hunger points:", result)
