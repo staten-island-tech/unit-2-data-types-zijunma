cdfc119f6e27e281a23eaccd1dac536e8ab25096
zijunma <158203969+zijunma@users.noreply.github.com>
2024-02-15 17:00:50 -0500

    Update hunger.py

--- hunger.py
+++ hunger.py
@@ -1,5 +1,18 @@
-def calculate_remaining_hunger_points(initial_hungerpoints, seconds):
-    remaining_hungerpoints = initial_hungerpoints - seconds
-    
+def calculate_remaining_hunger(initial_hungerpoints, seconds_waited, apples_eaten, hunger_gainedperapple, hunger_lostpersecond):
 
+# calc total number of hunger gained from eating apples
 
+totalhungerpointsgained = apples_eaten * initial_hungerpoints
+
+# calculate the hunger points lost when waiting
+ 
+total_hunger_points_lost = hunger_lostpersecond * seconds_waited
+
+# calculate the final hunger level
+
+final_hunger = totalhungerpointsgained - total_hunger_points_lost
+
+hunger_lostpersecond = int(input())
+initial_hungerpoints = int(input())
+seconds_waited = int(input())
+apples_eaten = int(input())
