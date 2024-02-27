49423cd030076b51ee1f67af55415ced425c8882
zijunma <158203969+zijunma@users.noreply.github.com>
2024-02-27 12:42:49 -0500

    main

--- homeroomsorter.py
+++ homeroomsorter.py
@@ -2,10 +2,6 @@ last_name = input("Whats your last name:")
 splitting_lastname = last_name.split( )
 first_initial = splitting_lastname[0]
 
-
-
-
-
 if first_initial == ("A"):
     print("Your homeroom is 101")
 elif first_initial == ("B"):
