f54fa9ca900f8a4dc03739a628c5ae8e21630586
zijunma <158203969+zijunma@users.noreply.github.com>
2024-03-07 12:29:15 -0500

    main

--- gradebook.py
+++ gradebook.py
@@ -1,14 +1,32 @@
-
-student = {
-    'name': "Sidh Gupta",
-    'class': "101",
-    'grades':[50,60,60]
-}
-
+# calculate grades 
 def calc_average(grades):
     total = sum(grades)
-    average = total % len(grades)
+    average = total / len(grades)
     return average
+#student inputs
+name = input("What is your name:")
+class_name = input("Whats your class:")
+# input grades
+grade_str = input("Enter grades, seperate with spaces: ")
+grade = [int(grade) for grade in grade_str.split()]
+
+#student dict
+student = {
+    'name': name,
+    'class_name': class_name, 
+    'grades': grade
+}
 
 average_grade = calc_average(student["grades"])
+
+#printing information
+print("Student Name:", name)
+print("Class:", class_name)
 print("Your average is:", average_grade)
+
+
+
+
+
+
+
