3f26df70e8b1be6fb6751c8a6977c6d73c63599b
zijunma <158203969+zijunma@users.noreply.github.com>
2024-03-18 11:56:32 -0400

    Update gradebook.py

--- gradebook.py
+++ gradebook.py
@@ -1,15 +1,42 @@
+def calculate_grades(grades):
+    if not grades:
+        return 0
+    return sum(grades) / len(grades)
 
-student = {
-"name": input("Whats your name:"),
-
-"class": input("Whats your class:"),
-
-"grades": int(input("Grades:"))
+student = { 
+    'name': '',
+    'class': '', 
+    'grade': '', 
 }
 
-def calc_average(grades):
-    total = sum(grades)
-    average = total % len(grades)
-    return average
+def createnewstudent(name, classes, grade):
+    avg_grades = calculate_grades(grade)
+    return { 
+        'name': name, 
+        'class': classes, 
+        'grades': grade,
+        'average': avg_grades 
+    }
+def zijun_idiot():
 
-average_grade = calc_average(student["grades"])
+    continue_makingstudents = input("Y")
+    student_list = []
+
+    while continue_makingstudents == "Y":
+        student_name = input("Enter the name:")
+        student_class = input("Enter your class:")
+        student_grades = input("Enter grades separated with commas:").split(',')
+        student_grades = [int(grade) for grade in student_grades]
+
+        new_student = createnewstudent(student_name, student_class, student_grades)
+        student_list.append(new_student)
+
+    for student in student_list:
+        print("Name:", student["name"])
+        print("Class:", student["class"])
+        print("Grades:", student["grades"])
+        print("Average:", student["average"])
+        print()
+
+continue_makingstudents = input("Do you want to keep adding more students:")
+print("Program has ended")
